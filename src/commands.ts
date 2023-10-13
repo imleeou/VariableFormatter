755f1c382305acbb04d70882b2279e70eab3c498
liufei <l1ufei@163.com>
2023-10-13 11:07:34 +0800

    feat: 添加快捷方式，切换驼峰命名格式

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,7 @@
 import { CommandType } from "./types";
 import { getCurrentWord, replaceCurrentWord } from "./utils";
 import { toCamelCase, toPascalCase, toSymbolJoin } from "./utils/convert";
+import { isCamelCase, isUpperCamelCase } from "./utils/judge";
 
 /** 自定义命令 */
 export const COMMANDS: CommandType[] = [
@@ -30,6 +31,19 @@ export const COMMANDS: CommandType[] = [
 		methods: () => {
 			const text = getCurrentWord();
 			replaceCurrentWord(toSymbolJoin(text, "_"));
+		}
+	},
+	{
+		name: "variableFormatter.toggleCamelCase",
+		methods: () => {
+			let text = getCurrentWord();
+			if (isCamelCase(text)) {
+				text = isUpperCamelCase(text) ? toCamelCase(text) : toPascalCase(text);
+			} else {
+				// 默认优先转换为小驼峰格式
+				text = toCamelCase(text);
+			}
+			replaceCurrentWord(text);
 		}
 	}
 ];
